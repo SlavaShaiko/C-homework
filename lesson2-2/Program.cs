9a180068aeb90b5c0a6b747c01f0ab691c4ed50a
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-07-14 11:50:59 +0300

    added lesson2-2

---
+++ lesson2-2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 13: Напишите программу, 
+которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if(numA<99) 
+{
+    Console.WriteLine($"{numA} -> третьей цифры нет");
+}
+else
+{
+    int resulA = numA % 10;
+    Console.WriteLine($"{numA} -> {resulA}");
+}
+
+
+
+
+
+
+
