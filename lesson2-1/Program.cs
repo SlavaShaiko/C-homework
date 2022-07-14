0c5c90397589a1f56c11bc823e2a87b93609cab3
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-07-14 11:31:38 +0300

    added lesson2-1

---
+++ lesson2-1/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 10: Напишите программу, 
+которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int resulA = numA / 10;
+int resulB = resulA % 10;
+Console.WriteLine($"{numA} -> {resulB}");
