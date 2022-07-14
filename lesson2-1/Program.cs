bff1ae7014a86ef87fa1b908375ae71c00d6f99d
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-07-14 11:38:01 +0300

    run lesson2-1

--- lesson2-1/Program.cs
+++ lesson2-1/Program.cs
@@ -5,9 +5,24 @@
 782 -> 8
 918 -> 1
 */
-Console.Clear();
+
+/*Console.Clear();
 Console.Write("Введите трёхзначное число: ");
 int numA = Convert.ToInt32(Console.ReadLine());
 int resulA = numA / 10;
 int resulB = resulA % 10;
 Console.WriteLine($"{numA} -> {resulB}");
+*/
+
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int MetodResul(int result)
+{
+    int resulA = result / 10;
+    int resulB = resulA % 10;
+    return resulB;
+}
+int metodResul = MetodResul(numA);
+Console.WriteLine($"{numA} -> {metodResul}");
