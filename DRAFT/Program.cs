2ff4841dc6f4c88017231df9bb62c8c88d0e1d40
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-07-15 11:10:57 +0300

    added draft

---
+++ DRAFT/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Clear();
+Console.Write("Введите цифру: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+static void Main(string[] numA)
+{
+    int n = 999454643;
+    int d = GetSecondDigit(n);
+    Console.WriteLine(d);
+    Console.ReadKey();
+}
+
+static int GetSecondDigit(int k)
+{
+    while (k >= 100) k /= 100;
+    int d = k % 10;
+    return d;
+}
