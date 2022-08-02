46af64d0a65211564f4a9c5a23607dad9f89d140
«SlavaShaiko» <v.shaiko2011@yandex.ru>
2022-08-02 20:34:54 +0300

    added Task 24

--- lessone4/Program.cs
+++ lessone4/Program.cs
@@ -1,5 +1,23 @@
-﻿
+﻿/*
+Задача 24: Напишите программу, которая
+принимает на вход число (А) и выдаёт сумму чисел
+от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
 
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int sum = 0;
+while (count <= a)
+{
+    sum = sum + count;
+    count++;
+}
+Console.WriteLine($"Сумма чисел от 1 до {a} -> {sum}");
 
 
 
