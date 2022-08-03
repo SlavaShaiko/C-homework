ffd7531919a377a544071aa6b609cf166a247d68
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-08-03 15:05:09 +0300

    added file lesson4

--- lessone4/Program.cs
+++ lessone4/Program.cs
@@ -23,7 +23,7 @@ int SumOfNumbers(int number)
 int result = SumOfNumbers(num);
 Console.WriteLine($"Сумма чисел от 1 до {num} -> {result}");
 */
-
+//__________________________________________________________________________________________________________
 /*
 Задача 26: Напишите программу, которая принимает
 на вход число и выдаёт количество цифр в числе.
@@ -49,9 +49,8 @@ int NumberOfDigits(int number)
 int number = NumberOfDigits(num);
 
 Console.WriteLine($"{num} -> {number}");
-
-//______________________________________________________________
-
+*/
+/*                     
 Console.Clear();
 Console.WriteLine("Введи число:");
 int a = Convert.ToInt32(Console.ReadLine());
@@ -70,19 +69,28 @@ int GetCountNum(int numb)
 int res = GetCountNum(a);
 Console.WriteLine($"Количество цифр в числе = {res}");
 */
-
+//__________________________________________________________________________________________________
 /*
 Задача 28: Напишите программу, которая
 принимает на вход число N и выдаёт
 произведение чисел от 1 до N.
 4 -> 24
 5 -> 120
-12 мин
 */
-
+/*
 Console.Clear();
 Console.WriteLine("Введи число:");
 int a = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+int index = 1;
+while (index <= a)
+{
+    result = result * index;
+    index++;
+}
+Console.WriteLine($"произведение чисел от 1 до {a} = {result}");
+*/
+/*
 int GetMulti(int numb)
 {
     int res = 1;
@@ -95,7 +103,59 @@ int GetMulti(int numb)
 
 int sum = GetMulti(a);
 Console.WriteLine($"произведение чисел от 1 до {a} = {sum}");
+*/
+/*
+Задача 30: Напишите программу, которая
+выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+/*
+Console.Clear();
 
+int[] array = new int[8];
+int rndNum = new Random().Next(0, 2);
+int count = array.Length;
+for (int i = 0; i < count; i++)
+{
+    array[i] = new Random().Next(0, 2);
+}
+Console.Write("[");
+for (int i = 0; i < count; i++)
+{
+    Console.Write(array[i]);
+    if (i != array.Length - 1) Console.Write(",");
+}
+Console.Write("]");
+*/
+Console.Clear();
+int[] array = GetRandomArray(8);
+WriteArrayToConsole(array);
+
+int[] GetRandomArray(int arrayLength)
+{
+    Random rnd = new Random();
+    int[] arr = new int[arrayLength];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+    return arr;
+}
+
+void WriteArrayToConsole(int[] array)
+{
+    Console.Clear();
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+            Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
 
 
 
