cf23b923ac29351c1675ce665082f3fc50a2a64f
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-08-06 10:02:12 +0300

    added new file draft

--- DRAFT/Program.cs
+++ DRAFT/Program.cs
@@ -572,3 +572,49 @@ namespace ConsoleApplication52
 //         }
 //     }
 // }
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        string letter = Convert.ToString(Console.ReadLine());
+
+        switch (letter)
+        {
+            case "а":
+                Console.WriteLine("гласная");
+                break;
+            case "о":
+                Console.WriteLine("гласная");
+                break;
+            case "э":
+                Console.WriteLine("гласная");
+                break;
+            case "е":
+                Console.WriteLine("гласная");
+                break;
+            case "и":
+                Console.WriteLine("гласная");
+                break;
+            case "ы":
+                Console.WriteLine("гласная");
+                break;
+            case "у":
+                Console.WriteLine("гласная");
+                break;
+            case "ё":
+                Console.WriteLine("гласная");
+                break;
+            case "ю":
+                Console.WriteLine("гласная");
+                break;
+            case "я":
+                Console.WriteLine("гласная");
+                break;
+
+            default:
+                break;
+        }
+    }
+}
