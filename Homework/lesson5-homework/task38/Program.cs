be97d918bc57a641e6877ddcad99bfbbae9e51e7
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-08-07 17:20:07 +0300

    added new file in task38

--- Homework/lesson5-homework/task38/Program.cs
+++ Homework/lesson5-homework/task38/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел.
+//  Найдите разницу между максимальным
+//   и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+Console.Clear();
+int[] GreateArrayRndDig(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+int[] ArrayRealNumber(int[] array)
+{
+    int i = 0;
+    int maxReal = 0;
+    int minReal = 0;
+    int count = array[i];
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j] < count) maxReal = array[j];
+        else minReal = array[j];
+    }
+    return new int[] { maxReal - minReal };
+}
+
+
+
+int[] greateArrayRndDig = GreateArrayRndDig(6, -9, 9);
+PrintArray(greateArrayRndDig);
+
+// Console.WriteLine($"сумма отрицательных чисел равна {sumPosNegDig[0]}");
+// Console.WriteLine($"сумма положительных чисел равна {sumPosNegDig[1],3}");
