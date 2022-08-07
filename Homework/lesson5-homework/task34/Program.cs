b8ad64f481ccd08725ca5199ea8aafa24108c482
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-08-07 14:43:34 +0300

    added working solution Task34

--- Homework/lesson5-homework/task34/Program.cs
+++ Homework/lesson5-homework/task34/Program.cs
@@ -1,3 +1,40 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+Console.Clear();
+int[] ArrayRnd(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i] + "]");
+    }
+}
+int[] ArrayPosDig(int[] array)
+{
+    int countPos = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            countPos++;
+        }
+    }
+    return new int[] { countPos };
+}
+
+int[] arrayRnd = ArrayRnd(10, 99, 1000);
+PrintArray(arrayRnd);
+int[] arrayPosDig = ArrayPosDig(arrayRnd);
+Console.WriteLine($" -> {arrayPosDig[0]}");
