4a18d7078ce35d9efb11dde261f03b903ce31790
«SlavaShaiko» <v.shaiko2011@yandex.ru>
2022-08-08 11:09:26 +0300

    added working solution task38

--- Homework/lesson5-homework/task38/Program.cs
+++ Homework/lesson5-homework/task38/Program.cs
@@ -26,22 +26,13 @@ void PrintArray(int[] array)
 
 int[] ArrayRealNumber(int[] array)
 {
-    int i = 0;
-    int maxReal = 0;
-    int minReal = 0;
-    int count = array[i];
-    for (int j = 0; j < array.Length; j++)
-    {
-        if (array[j] < count) maxReal = array[j];
-        else minReal = array[j];
-    }
-    return new int[] { maxReal - minReal };
+    int maxReal = array.Max();
+    int minReal = array.Min();
+    int differenceOfElements = maxReal - minReal;
+    return new int[] { differenceOfElements };
 }
 
-
-
-int[] greateArrayRndDig = GreateArrayRndDig(6, -9, 9);
+int[] greateArrayRndDig = GreateArrayRndDig(10, -99, 99);
 PrintArray(greateArrayRndDig);
-
-// Console.WriteLine($"сумма отрицательных чисел равна {sumPosNegDig[0]}");
-// Console.WriteLine($"сумма положительных чисел равна {sumPosNegDig[1],3}");
+int[] arrayRealNumber = ArrayRealNumber(greateArrayRndDig);
+Console.WriteLine($" -> {arrayRealNumber[0]}");
