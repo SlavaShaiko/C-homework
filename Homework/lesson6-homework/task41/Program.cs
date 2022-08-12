5708d315907a8a6c529d4e869f8ce92ac7c6cf68
«SlavaShaiko» <v.shaiko2011@yandex.ru>
2022-08-12 21:53:20 +0300

    added new folder lesson7

--- Homework/lesson6-homework/task41/Program.cs
+++ Homework/lesson6-homework/task41/Program.cs
@@ -3,41 +3,39 @@
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
 
-Console.Clear();
-Console.Write("Введите длину М: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int[] enteringNumbe = EnteringNumber(number);
-int[] EnteringNumber(int num)
-{
-    int ind = 0;
-    int[] arr = new int[num];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.Write($"Введите array [{i}]:");
-        ind = Convert.ToInt32(Console.ReadLine());
-        arr[i] = ind;
-    }
-    return arr;
-}
-void PrintNumber(int[] arra)
-{
-    for (int i = 0; i < arra.Length; i++)
-    {
-        if (i < arra.Length - 1) Console.Write(arra[i] + ", ");
-        else Console.Write($"{arra[i]} -> ");
-    }
-}
-PrintNumber(enteringNumbe);
-int NumberOfNumbers(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] > 0) count++;
-    }
-    return  count ;
-}
-int numberOfNumbers = NumberOfNumbers(enteringNumbe);
-Console.Write($"{numberOfNumbers}");
-
-
+// Console.Clear();
+// Console.Write("Введите длину М: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int[] enteringNumbe = EnteringNumber(number);
+// int[] EnteringNumber(int num)
+// {
+//     int ind = 0;
+//     int[] arr = new int[num];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write($"Введите array [{i}]:");
+//         ind = Convert.ToInt32(Console.ReadLine());
+//         arr[i] = ind;
+//     }
+//     return arr;
+// }
+// void PrintNumber(int[] arra)
+// {
+//     for (int i = 0; i < arra.Length; i++)
+//     {
+//         if (i < arra.Length - 1) Console.Write(arra[i] + ", ");
+//         else Console.Write($"{arra[i]} -> ");
+//     }
+// }
+// PrintNumber(enteringNumbe);
+// int NumberOfNumbers(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0) count++;
+//     }
+//     return  count ;
+// }
+// int numberOfNumbers = NumberOfNumbers(enteringNumbe);
+// Console.Write($"{numberOfNumbers}");
