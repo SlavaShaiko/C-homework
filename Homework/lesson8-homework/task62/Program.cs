c218cb506bd01ab1f5f382fbd1f1e02495d2a7c8
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-08-20 17:51:30 +0300

    new file

--- Homework/lesson8-homework/task62/Program.cs
+++ Homework/lesson8-homework/task62/Program.cs
@@ -22,21 +22,21 @@ int[,] NewTwoDimensionalArray(int line, int column)
     return array;
 }
 
-//Console.WriteLine("Заполняем массив числами от 1 до 16: ");
-//PrintArray(newArray);
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if (j == 0) Console.Write("[ ");
-            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],2}, ");
-            else Console.Write($"{array[i, j],2} ]");
-        }
-        Console.WriteLine();
-    }
-}
+// //Console.WriteLine("Заполняем массив числами от 1 до 16: ");
+// //PrintArray(newArray);
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (j == 0) Console.Write("[ ");
+//             if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],2}, ");
+//             else Console.Write($"{array[i, j],2} ]");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
 
 
