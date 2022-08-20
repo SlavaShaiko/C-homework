de16ed4e8c25f62ce903ec104f43e9d12ecba9fb
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-08-20 14:30:43 +0300

    added new file in folder lesson9

--- lesson9/Program.cs
+++ lesson9/Program.cs
@@ -23,41 +23,97 @@
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 6, 7, 8"
 
-Console.Clear();
-Console.Write("Введите число N:");
-int numberN = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число M:");
-int numberM = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// Console.Write("Введите число M:");
+// int numberM = Convert.ToInt32(Console.ReadLine());
 
-void NaturalNumber(int numM, int numN)
-{
-    if (numM == numN)
-        Console.Write($"{numM} ");
-    else if (numM < numN)
-        WriteNatural(numM, numN);
-    else
-        WriteNaturalJ(numN, numM);
-}
-NaturalNumber(numberM, numberN);
-Console.WriteLine();
+// Console.Write("Введите число N:");
+// int numberN = Convert.ToInt32(Console.ReadLine());
 
+// Console.Write($"M = {numberM}; N = {numberN} -> ");
+// NaturalNumber(numberM, numberN);
 
-void WriteNatural(int from, int to)
-{
-    if (from > to)
-        return;
-    Console.Write($"{from} ");
-    NaturalNumber(++from, to);
-}
-
-void WriteNaturalJ(int from, int to)
-{
-    if (from > to)
-        return;
-    Console.Write($"{to} ");
-    NaturalNumber(from, --to);
-}
-
-
+// void NaturalNumber(int m, int n)
+// {
+//     if (m == n)
+//     {
+//         Console.Write($"{m}");
+//         return;
+//     }
+//     else
+//     {
+//         if (m < n)
+//         {
+//             Console.Write(m + ",");
+//             NaturalNumber(m + 1, n);
+//             return;
+//         }
+//         else
+//         {
+//             if (m > n)
+//             {
+//                 Console.Write(m + ",");
+//                 NaturalNumber(m - 1, n);
+//                 return;
+//             }
+//         }
+//     }
+// }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// int res = SumReturnDigital(number, sum);
+// Console.Write($"Сумма цифр числа {number} -> {res} ");
+
+// int SumReturnDigital(int dig, int sum)
+// {
+//     if (dig == 0)
+//     {
+//         return sum;
+//     }
+//     else
+//     {
+//         sum += dig % 10;
+//         return SumReturnDigital(dig / 10, sum);
+//     }
+// }
+
+//----------------------------------------------------------------------------------
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.Clear();
+Console.Write("Введите число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число В:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b >= 0)
+{
+    int raisingNumber = RaisingNumber(a, b);
+    Console.Write($"A = {a}; B = {b} - > {raisingNumber}");
+}
+else
+{
+    Console.Write($"Неверная степень числа В:{b}");
+}
+
+int RaisingNumber(int a, int b)
+{
+    if (b == 0) return 1;
+    return RaisingNumber(a, b - 1) * a;
+}
